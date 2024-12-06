63ac63dad6933b28894cc5925c6cd809a494a801
Gabriel Hedman Slottner <gahe8576@rackham1.uppmax.uu.se>
2024-12-06 04:07:08 +0100

    Fixed model load order.

--- 03_Simulate_sequencing/Sequencing_simulation.sh
+++ 03_Simulate_sequencing/Sequencing_simulation.sh
@@ -10,7 +10,7 @@
 
 cat $0
 
-module load python/3.9.5 pysam/0.17.0-python3.9.5 bioinfo-tools biopython/1.80-py3.9.5 PBSIM3/3.0.4
+module load bioinfo-tools python/3.9.5 biopython/1.80-py3.9.5 pysam/0.17.0-python3.9.5 PBSIM3/3.0.4
 
 cd resultsPB
 
